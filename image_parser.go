bdfb0c609e3e7ba786bdda131e9f19595cd0d08f
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-18 14:46:31 -0700

    Move everything to one file

--- image_parser.go
+++ image_parser.go
@@ -89,6 +89,17 @@ func generateImage(path string, regions []Region) {
 	png.Encode(destinationFile, destinationImage)
 }
 
+// Determines similarity of colors (from 0 to 1). Higher - better
+func getDistanceBetweenColors(first color.Color, second color.Color) float64 {
+	f := first.(color.RGBA)
+	s := second.(color.RGBA)
+
+	distance := math.Pow(float64(f.R) - float64(s.R), 2) + 
+	 			math.Pow(float64(f.G) - float64(s.G), 2) +
+	 			math.Pow(float64(f.B) - float64(s.B), 2)
+
+	 return 1 - distance / (255 * 255 * 3)
+}
 
 func main() {
 	fBaseImg, _ := os.Open("image.png")
