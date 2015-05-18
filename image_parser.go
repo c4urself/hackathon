88c0eb4c77b03295aec6a1b8cc086e5a9cfb024a
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-18 11:33:51 -0700

    Splitting by regions should work now

--- image_parser.go
+++ image_parser.go
@@ -3,31 +3,48 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/draw"
-	"image/jpeg"
 	"os"
+	"math"
 )
+import _ "image/png"
+
+type Region struct {
+	img image.Image
+	offset image.Point
+}
+
+func getImageRegions(img image.Image, regionLength int) []Region {
+	var imgSize image.Point = img.Bounds().Size()
+	var xPointsCount int = int(math.Ceil(float64(imgSize.X) / float64(regionLength)))
+	var yPointsCount int = int(math.Ceil(float64(imgSize.Y) / float64(regionLength)))
+	var regions []Region;
+	var regionImg image.Image;
+	var regionMinOffset image.Point;
+	var regionMaxOffset image.Point;
+
+	rgbImage := img.(*image.RGBA)
+
+	for i := 0; i < yPointsCount; i++ {
+		for j := 0; j < xPointsCount; j++ {
+			regionMinOffset = image.Point{X: j * regionLength, Y: i * regionLength}
+			regionMaxOffset = image.Point{X: (j + 1) * regionLength, Y: (i + 1) * regionLength}
+			regionImg = rgbImage.SubImage(image.Rectangle{Min: regionMaxOffset, Max: regionMaxOffset})
+			regions = append(regions, Region{img: regionImg, offset: regionMinOffset})
+		}
+	}
+
+	return regions
+}
 
 func main() {
-
-	fBaseImg, _ := os.Open("image.jpeg")
+	fBaseImg, _ := os.Open("image.png")
 	defer fBaseImg.Close()
 	baseImg, _, err := image.Decode(fBaseImg)
+	
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	// image.RGBA is an in-memory image
-	// image.Rect returns type Rectangle
-	m := image.NewRGBA(image.Rect(0, 0, 20, 20))
-	var rect image.Rectangle
-	rect = m.Bounds()
-	draw.Draw(m, rect, baseImg, image.Point{0, 0}, draw.Src)
-
-	fResultImg, _ := os.Create("new.jpeg")
-	defer fResultImg.Close()
-
-	jpeg.Encode(fResultImg, m, &jpeg.Options{jpeg.DefaultQuality})
-
+	fmt.Print(getImageRegions(baseImg, 40));
 }
