bbbaf536810e7f947ce4b7340ffa6e7519621ef4
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-19 11:29:24 -0700

    Return app initialisation code

--- app.go
+++ app.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github.com/c4urself/hackathon/mosaic"
-	// "github.com/c4urself/hackathon/web"
+	// "fmt"
+	// "github.com/c4urself/hackathon/mosaic"
+	"github.com/c4urself/hackathon/web"
 )
 
 func main() {
-	fmt.Println(mosaic.MakeInstagramMosaic("pewdiepie", "/tmp/pewdiepie/photos", "/tmp/pewdiepie/audience", "/tmp/pewdiepie/mosaic/"))
+	// fmt.Println(mosaic.MakeInstagramMosaic("pewdiepie", "/tmp/pewdiepie/photos", "/tmp/pewdiepie/audience", "/tmp/pewdiepie/mosaic/"))
 	// mosaic.MakeMosaic("image.png", "./thumbnails/", "result.png", 10)
-	// web.StartApp()
+	web.StartApp()
 }
