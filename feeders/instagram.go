9e1a7666fb5402d81132b81a7ec0fc0073e772b5
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-19 10:37:34 -0700

    Base workflow for instagram should work now

--- feeders/instagram.go
+++ feeders/instagram.go
@@ -1,26 +1,43 @@
-package main
+package feeders
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"github.com/jeffail/gabs"
 	"net/http"
+	"image/jpeg"
+ 	"image/png"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 )
 
-type CreatorPhoto struct {
-	url string
-	likes int64
+type Photo struct {
+	Id string
+	Url string
+	Likes int64
 }
 
 type CreatorFeed struct {
-	photos []CreatorPhoto
-	audience []string
+	Photos []Photo
+	Audience []Photo
+}
+
+func NewPhoto(url string, likes int64) Photo {
+	return Photo{Url: url, Id: Hash(url), Likes: likes}
+}
+
+func Hash(s string) string {
+	hash := md5.Sum([]byte(s))
+	return hex.EncodeToString(hash[:])
 }
 
 // Fetches photos/auedience from Instagram by username
-func getCreatorFeed(username string) CreatorFeed {
+func GetCreatorFeed(username string) CreatorFeed {
 	resp, err := http.Get(fmt.Sprintf("https://instagram.com/%s/media/", username))
 	defer resp.Body.Close()
 
@@ -44,13 +61,14 @@ func getCreatorFeed(username string) CreatorFeed {
 	// Fetch profile photos
 	items, _ := jresp.Search("items").Children()
 
-	var creatorPhotos []CreatorPhoto
+	var creatorPhotos []Photo
 	var audiencePhotos map[string]bool = make(map[string]bool, len(items) * 50)
 
 	for _, item := range items  {
 		url := item.Path("images.standard_resolution.url").String()
+		url = strings.Trim(url, "\"")
 		likes, _ := strconv.ParseInt(item.Path("likes.count").String(), 10, 64)
-		creatorPhotos = append(creatorPhotos, CreatorPhoto{url: url, likes: likes})
+		creatorPhotos = append(creatorPhotos, NewPhoto(url, likes))
 
 		// Parse likers
 		likers, _ := item.Path("likes.data.profile_picture").Children()
@@ -66,12 +84,40 @@ func getCreatorFeed(username string) CreatorFeed {
 	}
 
 	// Audience includes only unique urls and doesn't include placeholder photo
-	audience := make([]string, len(audiencePhotos))
+	var audience []Photo
 	for k := range audiencePhotos {
+		k = strings.Trim(k, "\"")
 		if k != "https://instagramimages-a.akamaihd.net/profiles/anonymousUser.jpg" {
-			audience = append(audience, k)
+			audience = append(audience, NewPhoto(k, 0))
 		}
 	}
 
-	return CreatorFeed{photos: creatorPhotos, audience: audience}
+	return CreatorFeed{Photos: creatorPhotos, Audience: audience}
+}
+
+func LoadPhotos(photos []Photo, baseDir string) {
+	for _, photo := range photos {
+		log.Printf("Loading photo %s", photo.Url)
+
+		fPath := filepath.Join(baseDir, fmt.Sprintf("%s.png", photo.Id))
+		f, err := os.Create(fPath)
+		defer f.Close()
+		if err != nil {
+			log.Printf("Unable to create photo for loading %s, %s", fPath, err)
+			continue
+		}
+
+		resp, err := http.Get(photo.Url)
+		if err != nil {
+			log.Printf("Unable to fetch photo %s, %s", photo.Url, err)
+			continue
+		}
+
+		img, err := jpeg.Decode(resp.Body)
+		if err != nil {
+			log.Printf("Unable to convert jpg -> png %s, %s", photo.Url, err)
+		}
+
+		png.Encode(f, img)
+	}
 }
--- mosaic/flows.go
+++ mosaic/flows.go
@@ -1,10 +1,12 @@
 package mosaic
 
 import (
+	"github.com/c4urself/hackathon/feeders"
 	"fmt"
 	"image"
 	"image/png"
 	"os"
+	"path/filepath"
 )
 
 func MakeMosaic(mainPath string, thumbnailsPath string, mosaicPath string, tileSize int) {
@@ -27,4 +29,22 @@ func MakeMosaic(mainPath string, thumbnailsPath string, mosaicPath string, tileS
 	mosaicFile, _ := os.Create(mosaicPath)
 	defer mosaicFile.Close()
 	png.Encode(mosaicFile, mosaicImage)
+}
+
+func MakeInstagramMosaic(username string, photosDir string, audienceDir string, mosaicDir string) {
+	os.MkdirAll(photosDir, 0777)
+	os.MkdirAll(audienceDir, 0777)
+	os.MkdirAll(mosaicDir, 0777)
+
+	feed := feeders.GetCreatorFeed(username)
+
+	topPhotos := feed.Photos[:5]
+	feeders.LoadPhotos(topPhotos, photosDir)
+	feeders.LoadPhotos(feed.Audience, audienceDir)
+
+	for _, photo := range topPhotos {
+		photoPath := filepath.Join(photosDir, fmt.Sprintf("%s.png", photo.Id))
+		mosaicPath := filepath.Join(mosaicDir, fmt.Sprintf("%s.png", photo.Id))
+		MakeMosaic(photoPath, audienceDir, mosaicPath, 10)
+	}
 }
