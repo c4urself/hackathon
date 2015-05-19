db7e7dc7b5fca3d2404c9cc397ee64f1ebcabf09
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-18 18:54:10 -0700

    Shape project structure

--- mosaic/flows.go
+++ mosaic/flows.go
@@ -3,23 +3,28 @@ package mosaic
 import (
 	"fmt"
 	"image"
-	"log"
+	"image/png"
 	"os"
 )
 
-func MakeMosaic(mainPath string, thumbnailsPath string, mosaicPath string) {
-	fBaseImg, _ := os.Open(mainPath)
-	defer fBaseImg.Close()
-	baseImg, _, err := image.Decode(fBaseImg)
-
+func MakeMosaic(mainPath string, thumbnailsPath string, mosaicPath string, tileSize int) {
+	// Import base image
+	baseImgFile, _ := os.Open(mainPath)
+	defer baseImgFile.Close()
+	baseImg, _, err := image.Decode(baseImgFile)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	var regions []Region = getImageRegions(baseImg, 40)
-	log.Printf("Created %v regions", len(regions))
+	// Generate mosaic
+	originalTiles := BreakToTiles(baseImg, tileSize)
+	candidateTiles := ImportTiles(thumbnailsPath, tileSize)
+	similarTiles := FindSimilarTiles(originalTiles, candidateTiles) 
+	mosaicImage := CollectFromTiles(similarTiles)
 
-	result := matchRegions(regions, generateImageSet(thumbnailsPath))
-	generateImage(mosaicPath, result)
+	// Write result
+	mosaicFile, _ := os.Create(mosaicPath)
+	defer mosaicFile.Close()
+	png.Encode(mosaicFile, mosaicImage)
 }
