8f148b034389b46f52417af16fe5b59f6c28a92d
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-19 21:39:26 -0700

    Enable navigation

--- static/js/result.js
+++ static/js/result.js
@@ -1,6 +1,7 @@
 $(function() {
 	var photos = $(".photo"),
-	    mosaic = $(".mosaic .mat img");
+	    mosaic = $(".mosaic .mat img"),
+	    search = $(".search");
 
 	function switchPhoto(photo, speed) {
 		mosaic.fadeOut(speed, function() {
@@ -14,6 +15,11 @@ $(function() {
 	photos.click(function() {
 		switchPhoto($(this), 300);
 	});
+
+	search.submit(function(e) {
+		e.preventDefault();
+		document.location = "/mosaic/" + search.find("input").val();
+	})
 
 	switchPhoto(photos.first(), 0);
 });
