165798e0ab0f48027409c18ff145135047b6e818
Dmytro Popovych <drudim.ua@gmail.com>
2015-05-19 22:24:53 -0700

    Use average RGB to compair tiles

--- mosaic/colors.go
+++ mosaic/colors.go
@@ -1,43 +1,31 @@
 package mosaic
 
 import (
-	"fmt"
 	"github.com/nfnt/resize"
 	"image"
 	"image/color"
 	"math"
-	"strconv"
 )
 
 func GetColorProfile(img image.Image) color.NRGBA {
-
-	var colorMap = make(map[string]int)
-	var length uint = 10
-
 	// create a thumb to shortcut getting most common colours
+	var length uint = 10
 	var thumb image.Image = resize.Thumbnail(length, length, img, resize.NearestNeighbor)
+
+	var r, g, b, count int64 = 0, 0, 0, 0
 
 	// for each pixel add to a map
 	for x := 0; x < int(length); x++ {
 		for y := 0; y < int(length); y++ {
 			c := color.NRGBAModel.Convert(thumb.At(x, y)).(color.NRGBA)
-			var hex string = RGBToHex(c.R, c.G, c.B)
-			colorMap[hex] += 1
+			r += int64(c.R)
+			g += int64(c.G)
+			b += int64(c.B)
+			count += 1
 		}
 	}
 
-	// for each key, val in map return highest val
-	var baseColor string
-	var highest = 0
-	for k, v := range colorMap {
-		if v > highest {
-			highest = v
-			baseColor = k
-		}
-	}
-
-	r, g, b := HexToRGB(baseColor)
-	return color.NRGBA{r, g, b, 255}
+	return color.NRGBA{uint8(r / count), uint8(g / count), uint8(b / count), 255}
 }
 
 func GetColorDistance(a color.NRGBA, b color.NRGBA) float64 {
@@ -46,23 +34,4 @@ func GetColorDistance(a color.NRGBA, b color.NRGBA) float64 {
 		math.Pow(float64(a.B)-float64(b.B), 2)
 
 	return distance / (255 * 255 * 3)
-}
-
-func RGBToHex(r, g, b uint8) string {
-	return fmt.Sprintf("#%02X%02X%02X", r, g, b)
-}
-
-func HexToRGB(h string) (uint8, uint8, uint8) {
-	if len(h) > 0 && h[0] == '#' {
-		h = h[1:]
-	}
-	if len(h) == 3 {
-		h = h[:1] + h[:1] + h[1:2] + h[1:2] + h[2:] + h[2:]
-	}
-	if len(h) == 6 {
-		if rgb, err := strconv.ParseUint(string(h), 16, 32); err == nil {
-			return uint8(rgb >> 16), uint8((rgb >> 8) & 0xFF), uint8(rgb & 0xFF)
-		}
-	}
-	return 0, 0, 0
 }
